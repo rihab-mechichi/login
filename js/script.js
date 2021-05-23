fa639984276ecafc577a32a644943099f68fae49
mechichi rihab <rihab.mechichi@gmail.com>
2021-05-23 17:12:10 +0100

    ajout de tt les fichiers

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var btn4 = document.getElementById('suiF');
+//console.log(btn4)
+btn4.addEventListener('click',function(){
+    alert('Félicitations vous avez maintenant un compte, connectez-vous!');
+});
